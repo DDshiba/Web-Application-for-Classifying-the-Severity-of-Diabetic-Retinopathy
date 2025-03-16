b0c6fff0f88b49f3a72144242a5b9dd1d1571f37
DDshiba <thitikotnindee@gmail.com>
2025-03-16 14:51:25 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./component/Footer/Footer";
 import Nav from "./component/Nav/Nav";
 import Home from "./layout/Home";
 import Steps from "./layout/Steps";
 import MoreInfo from "./layout/MoreInfo";
-import Process from "./layout/Process"; // เปลี่ยนชื่อไฟล์ที่นำเข้า
-import ScrollToTop from "./component/ScrollToTop/ScrollToTop"; // นำเข้า ScrollToTop
+import Process from "./layout/Process"; 
+import ScrollToTop from "./component/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
-    <>
-      <Router>
-        <ScrollToTop /> {/* ใช้ที่นี่ */}
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/steps" element={<Steps />} />
-          <Route path="/more-info" element={<MoreInfo />} />
-          <Route path="/process" element={<Process />} />{" "}
-          {/* อัปเดตชื่อ Route */}
-        </Routes>
-        <Footer />
-      </Router>
-    </>
+    <Router>
+      <ScrollToTop />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/steps" element={<Steps />} />
+        <Route path="/more-info" element={<MoreInfo />} />
+        <Route path="/process" element={<Process />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
